ac8c1cf5056e7c8910fe382fedd62d668ac7ea28
ReinisRu <rr@itfly.lv>
2021-01-14 16:52:40 +0200

    dropdown full height, hover effect, bacgr dimm add

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-navButton = document
-  .getElementById(`collapse`)
-  .addEventListener(`click`, function () {
-    document.getElementById(`collapse1`).classList.toggle("hidden");
-  });
+document.getElementById(`collapse`).addEventListener(`click`, function () {
+  document.getElementById(`dropdown`).classList.toggle("hidden");
+  document.getElementById(`cover`).classList.toggle("hidden");
+});
+document.getElementById(`cover`).addEventListener(`click`, function () {
+  document.getElementById(`dropdown`).classList.toggle("hidden");
+  document.getElementById(`cover`).classList.toggle("hidden");
+});
+
+// Otrs variants priekš "hidden" toggle
+
+// document.querySelectorAll(".collapse").forEach((item) => {
+//   item.addEventListener("click", function () {
+//     document.querySelectorAll(".cover").forEach((el) => {
+//       el.classList.toggle("hidden");
+//     });
+//   });
+// });
