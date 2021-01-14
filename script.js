ac3b567a1103e21ee918210888c0c72f60e9a1bc
ReinisRu <rr@itfly.lv>
2021-01-14 20:45:07 +0200

    Shorter js for Navigation Btn & cover

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-document.getElementById(`collapse`).addEventListener(`click`, function () {
-  document.getElementById(`dropdown`).classList.toggle("hidden");
-  document.getElementById(`cover`).classList.toggle("hidden");
-});
-document.getElementById(`cover`).addEventListener(`click`, function () {
-  document.getElementById(`dropdown`).classList.toggle("hidden");
-  document.getElementById(`cover`).classList.toggle("hidden");
+document.querySelectorAll("#navBtn, #cover").forEach((e) => {
+  e.addEventListener("click", function () {
+    document.querySelectorAll("#dropdown, #cover").forEach((el) => {
+      el.classList.toggle("hidden");
+    });
+  });
 });
 
-// Otrs variants priekš "hidden" toggle
+/* Garāks variants bez arrow function */
 
-// document.querySelectorAll(".collapse").forEach((item) => {
-//   item.addEventListener("click", function () {
-//     document.querySelectorAll(".cover").forEach((el) => {
-//       el.classList.toggle("hidden");
-//     });
-//   });
+// document.getElementById(`collapse`).addEventListener(`click`, function () {
+//   document.getElementById(`dropdown`).classList.toggle("hidden");
+//   document.getElementById(`cover`).classList.toggle("hidden");
+// });
+// document.getElementById(`cover`).addEventListener(`click`, function () {
+//   document.getElementById(`dropdown`).classList.toggle("hidden");
+//   this.classList.toggle("hidden");
 // });
