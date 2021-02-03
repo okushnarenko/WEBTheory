8c450bc56c9c49194ec2c5222130d0a8c6085672
ReinisRu <rr@itfly.lv>
2021-02-03 16:10:36 +0200

    JS submit Answer listener & save Locally added

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ document.querySelectorAll("#navBtn, #cover").forEach((e) => {
     });
   });
 });
+
+// (Reinis) iesniegt Atbildi poga:
+document.querySelector(".for-fun").addEventListener("submit", function (event) {
+  // event.preventDefault();
+  let question = document.getElementById("questionText").innerText;
+  console.log(question); /* var izdzēst pēc testēšanas */
+  let answer = this.querySelector("input[name = answer]:checked").value;
+  console.log(answer); /* var izdzēst pēc testēšanas */
+  sessionStorage.setItem(question, answer);
+  /* varam izvēlēties glabāt Session Storage vai Local Storage */
+  localStorage.setItem(question, answer);
+});
